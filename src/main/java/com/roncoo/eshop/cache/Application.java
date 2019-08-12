797421acb272fbb6c0d61a7c3bfd7a978082e5a4
liuxi58 <liuxi58@crland.com.cn>
2019-08-12 19:41:29 +0800

    t

--- src/main/java/com/roncoo/eshop/cache/Application.java
+++ src/main/java/com/roncoo/eshop/cache/Application.java
@@ -1,6 +1,7 @@
 package com.roncoo.eshop.cache;
 
 import com.roncoo.eshop.cache.kafka.KafkaSender;
+import com.roncoo.eshop.cache.listener.InitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
